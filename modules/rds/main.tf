b9d3fec79370c0f422dc9167a19b29f61d520a25
JorgeNeir <jorge.neira88@gmail.com>
2026-05-23 18:09:26 -0500

    revert: RDS public access no longer needed (pages are now dynamic)

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -17,7 +17,6 @@ resource "aws_db_instance" "postgres" {
   backup_window           = "03:00-04:00"
   maintenance_window      = "mon:04:00-mon:05:00"
 
-  publicly_accessible   = true
   vpc_security_group_ids = [aws_security_group.rds.id]
 
   db_subnet_group_name = aws_db_subnet_group.main.name
