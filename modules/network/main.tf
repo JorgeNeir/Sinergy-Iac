a73abe0bf1141813a68dbdd60071ea8c92524ed0
JorgeNeir <jorge.neira88@gmail.com>
2026-05-23 13:02:01 -0500

    feat: make RDS publicly accessible for CI builds

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -52,3 +52,13 @@ resource "aws_route_table" "public" {
     Name        = "${var.app_name}-public-rt"
   }
 }
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.public.id
+}
