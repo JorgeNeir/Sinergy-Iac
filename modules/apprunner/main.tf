79b1e491e213a41f9373018bc33b3db524fdecfe
JorgeNeir <jorge.neira88@gmail.com>
2026-05-23 19:29:51 -0500

    fix: allow all outbound traffic from App Runner VPC connector SG

--- modules/apprunner/main.tf
+++ modules/apprunner/main.tf
@@ -14,13 +14,6 @@ resource "aws_security_group" "apprunner_vpc" {
   description = "Security group for App Runner VPC connector"
   vpc_id      = var.vpc_id
 
-  egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Environment = var.environment
     Name        = "${var.app_name}-apprunner-vpc-sg"
